22d65b617dd7f8592e23c6abea8fd4a409fa5b6d
MalinDulkies <malin.dulkies@yoc.com>
2022-01-21 15:34:58 +0100

    First steps in data fetching

--- src/components/imgFetch/imgFetch.tsx
+++ src/components/imgFetch/imgFetch.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 
 
 
 function IMGFetch(props) {
 
-    const url = props.src;
-
-    return (
-        <img src={url} ></img>
-    );
+        const [state, setState] = useState([])
+        const [query, setQuery] = useState()
+        useEffect(() => {
+            fetch("/api/data?q=" + query).then(
+                res => setState(res.data)
+            )
+        }, [query])
+        function searchQuery(evt) {
+            const value = evt.target.value
+            setQuery(value)
+        }
+        return (
+            <>
+                {state.map( d => <div>{d}</div>)}<input type="text" placeholder="Type your query" onEnter={searchQuery} />  
+            </>
+        )
+    }
 }
 export default IMGFetch;
